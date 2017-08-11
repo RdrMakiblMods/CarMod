e2b8a96f756dc8c63d1c7fddc7fda23ef10878c0
rdrpenguin04 <rdrpenguin04@users.noreply.github.com>
2017-08-11 11:31:49 -0500

    Initial Commit from Eclipse.

---
+++ MDKExample/src/main/java/com/makibl/carmod/CarMod.java
@@ -0,0 +1,8 @@
+package com.makibl.carmod;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid=Constants.MODID, name=Constants.NAME, version=Constants.VERSION)
+public class CarMod {
+	
+}
---
+++ MDKExample/src/main/java/com/makibl/carmod/Constants.java
@@ -0,0 +1,7 @@
+package com.makibl.carmod;
+
+public class Constants {
+	public static final String MODID = "makiblcarmod";
+	public static final String NAME = "Makibl's Car Mod";
+	public static final String VERSION = "1.0.0";
+}
